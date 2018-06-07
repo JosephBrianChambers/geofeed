d47e11293f34ebe50ccd39f7681cb899539372e6
Joe Chambers <josephbrianchambers@gmail.com>
2018-06-07 13:49:22 -0700

    Add basic styling frame work: css modules

--- app/javascript/app-entry/views/layout-container.jsx
+++ app/javascript/app-entry/views/layout-container.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom'
 import { withRouter } from 'react-router-dom';
+import styles from './layout-container-styles'
 
 class LayoutContainer extends React.Component {
   constructor(props) {
@@ -11,14 +12,14 @@ class LayoutContainer extends React.Component {
   render() {
     return (
       <div>
-        <header>
+        <header className={styles.header}>
           <h2>Header</h2>
           <Link to="/login">Login</Link>
         </header>
-        <main>
+        <main className={styles.main}>
           {this.props.children}
         </main>
-        <footer>footer</footer>
+        <footer className={styles.footer}>footer</footer>
       </div>
     );
   }
